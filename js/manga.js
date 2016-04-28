432213116a36624b740de1a839b3b8447aa4ad10
Safery Hossain <janingpower@yahoo.com>
2016-04-28 18:43:30 -0400

    Update manga.js

--- js/manga.js
+++ js/manga.js
@@ -60,9 +60,13 @@ function Detail(url){
 				var cell2 = row.insertCell(1);
 				var cell3 = row.insertCell(2);
 
-				cell1.innerHTML = myArr["chapters"][key][2];
+				cell1.innerHTML = "<a href='view.html?fname=Rothshi&id=" + myArr["chapters"][key][3] + "'><b>" + myArr["chapters"][key][2] + "</b></a>";
 				cell2.innerHTML = myArr["chapters"][key][0];
-				cell3.innerHTML = myArr["chapters"][key][3];
+				// Get Publish time.
+				var get_time = convertTimestamp(myArr["chapters"][key][1]);
+
+
+				cell3.innerHTML = get_time;
 				row_counter = row_counter + 1;
 			}
 			// Function to create generate all the descriptions.
@@ -78,4 +82,95 @@ function GenerateFunct(){
 	var get_id = getQueryVariable('id');
 	// Create all the chapters table.
 	var manga_detail = Detail("https://www.mangaeden.com/api/manga/" + get_id);
+}
+
+// Function to create all the Manga picture when users chooses a chapter.
+function CreateMangas(){
+	var get_id = getQueryVariable('id');
+	var url = "https://www.mangaeden.com/api/chapter/" + get_id;
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			var myArr = JSON.parse(xhttp.responseText);
+			var img_url = "https://cdn.mangaeden.com/mangasimg/";
+			// Tell user to choose a page to read.
+			var myDiv = document.getElementById("sel");
+			var selectList = document.createElement("option");
+			selectList.innerHTML = "Please choose a page to read from this list."
+			selectList.value = "stop";
+			myDiv.appendChild(selectList);
+
+			// Insert all the chapters and picture
+			for (key in myArr["images"]){
+				//Create and append select list
+				var selectList = document.createElement("option");
+				selectList.innerHTML = myArr["images"][key][0];
+				selectList.value = myArr["images"][key][1];
+				myDiv.appendChild(selectList);
+			}
+		}
+	};
+	xhttp.open("GET", url, true);
+	xhttp.send();
+}
+
+// Function to generate chapter images.
+function update_manga(got_id){
+	var CreateImg = new Image();
+	var getDiv = document.getElementById("manga_pics");
+	$("#manga_pics").empty() // Empty the previous image.
+	if (got_id == "stop"){
+		var CreateH1 = document.createElement("h1");
+		CreateH1.innerHTML = "<center>Please select page to read from the top drop-down box.</center>"
+		getDiv.appendChild(CreateH1);
+
+	}
+	else{
+		CreateImg.src = 'https://cdn.mangaeden.com/mangasimg/' + got_id;
+		CreateImg.id = "chapter_img";
+		getDiv.appendChild(CreateImg);
+	}
+}
+
+function Prev_Page(){
+	$('#sel option:selected').next().attr('selected', 'selected');
+	if ($('#sel').val() != "stop"){
+		update_manga($('#sel').val());
+	}
+
+}
+
+function Next_Page(){
+	$('#sel option:selected').prev().attr('selected', 'selected');
+	if ($('#sel').val() != "stop"){
+		update_manga($('#sel').val());
+	}
+
+}
+
+function convertTimestamp(timestamp) {
+  var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
+		yyyy = d.getFullYear(),
+		mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+		dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+		hh = d.getHours(),
+		h = hh,
+		min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+		ampm = 'AM',
+		time;
+			
+	if (hh > 12) {
+		h = hh - 12;
+		ampm = 'PM';
+	} else if (hh === 12) {
+		h = 12;
+		ampm = 'PM';
+	} else if (hh == 0) {
+		h = 12;
+	}
+	
+	// ie: 2013-02-18, 8:35 AM	
+	time = yyyy + '-' + mm + '-' + dd;
+		
+	return time;
 }
