f75ba96e81c3627377ddfd01f1e5964e811c430a
Safery Hossain <janingpower@yahoo.com>
2016-04-28 23:45:49 -0400

    Update manga.js

--- js/manga.js
+++ js/manga.js
@@ -174,3 +174,64 @@ function convertTimestamp(timestamp) {
 		
 	return time;
 }
+
+function Random_Create(){
+	alert("hi");
+}
+
+function GenerateAns(type){
+	if (type == "random"){
+		$("#secret").empty();
+		var search_url = "https://www.mangaeden.com/api/list/0/";
+		// Ajax request
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			var myArr = JSON.parse(xhttp.responseText);
+			var img_url = "https://cdn.mangaeden.com/mangasimg/";
+			var count = 1;
+			var div = document.getElementById('front');
+			while (count <= 10){
+				var img = new Image();
+				var key = Math.floor(Math.random() * 16950) + 1;
+				// Main Div file.
+				var Main_Div = document.createElement("div");
+				var CreateA = document.createElement("a");
+				CreateA.href = "pages/manga.html?fname=Rothshi&id=" + myArr["manga"][key]["i"];
+				Main_Div.className = "wrapper";
+				img.src = img_url + myArr["manga"][key]["im"];
+				// Create the thumbnail for each images.
+				img.className = "img-thumbnail";
+				CreateA.appendChild(img);				
+				// Create div for the description.
+				var divDesc = document.createElement("p");
+				divDesc.innerHTML = myArr["manga"][key]["t"].slice(0, 20); // Name of the Manga
+				divDesc.className = "desc_content";
+				CreateA.appendChild(divDesc)
+				Main_Div.appendChild(CreateA);
+				// Insert the images inside the main div picture element.
+				div.appendChild(Main_Div);
+				count = count + 1;
+				}
+			}
+		};
+	xhttp.open("GET", search_url, true);
+	xhttp.send();
+	}
+	else if (type == "fav"){
+		$("#secret").empty();
+		var key = Math.floor(Math.random() * 8) + 1;
+		var counter = 0;
+		while (counter <= key){
+			var h1 = document.createElement("h1");
+			var get_div = document.getElementById("front");
+			h1.innerHTML = "<center>WE LOVE ALL THE MANGA</center>";
+			get_div.appendChild(h1);
+			counter = counter + 1;
+		}
+
+	}
+	else if (type == "EC"){
+		$("#secret").show();
+	}
+}
